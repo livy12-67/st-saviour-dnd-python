1415a17ba2c7944cd24d67f3051091cdbf19f991
ocrammer <ocrammer28@stsaviour.org>
2025-11-23 21:27:47 -0500

    my trivia game

--- game.py
+++ game.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     answer = input('Question 1: What is the largest animal on earth?')
     if answer == 'Whale':
         print('Correct good job!')
-        score += 2 # type: ignore
+        score+=2
     else: 
         print ('Sorry... that is wrong.')
 
