dc0e8715963d04da18d743ef1282b8f87ff7e957
ocrammer <ocrammer28@stsaviour.org>
2025-11-23 19:20:22 -0500

    my trivia game

--- game.py
+++ game.py
@@ -18,9 +18,9 @@ if __name__ == '__main__':
 
   
     print_dramatic_text('Welcome to my trivia game!') 
-    print_dramatic_text ('Name:')
+    input = ('Name:')
 
-    
+
     answer = input('Question 1: What is the largest animal on earth?')
     if answer == 'Whale':
         print('Correct good job!')
